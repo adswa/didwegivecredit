44aae1569874ec764e6dc0ee6d4d7f4780c21354
Adina Wagner <adina.wagner@t-online.de>
2020-06-29 14:57:38 +0200

    strip python 3.5 - rich dependency requires > 3.6

--- setup.py
+++ setup.py
@@ -20,14 +20,13 @@ test_requirements = ['pytest>=3', ]
 setup(
     author="Adina Wagner",
     author_email='adina.wagner@t-online.de',
-    python_requires='>=3.5',
+    python_requires='>=3.6',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
@@ -49,6 +48,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/adswa/didwegivecredit',
-    version='0.0.1',
+    version='0.0.2',
     zip_safe=False,
 )
